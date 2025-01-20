a7e6ecd6cf038a7b55ef0b44dbbd975220b31931
AnikaTahshin <anikatahsinmeem@gmail.com>
2025-01-20 15:51:39 +0600

    navbar on process

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+"use client"
+import Image from "next/image";
+import React from "react";
+import logo from '../../public/assets/logo.png'
+import  '../../styles/css/navbar.css'
+import  '../../styles/responsive/responsive.css'
+
+import Link from 'next/link'
+
+
+const Navbar = () => {
+
+  
+const navlist = [
+  {
+    id:1,
+    name:"Home",
+
+  },
+
+  {
+    id:2,
+    name:"About",
+
+  },{
+    id:3,
+    name:"Services",
+
+  },{
+    id:4,
+    name:"Projects",
+
+  },{
+    id:5,
+    name:"Testimonial",
+
+  },{
+    id:6,
+    name:"Contact",
+
+  }
+]
+  return (
+    <div className="nav-container-div">
+      <div>
+        <Image className="logo-img" src={logo} height={100} width={150} alt="" />
+      </div>
+      
+      <div className="nav-list-container">
+        {
+          navlist.map((item) => (
+            <li className="nav-list" key={item.id}>
+            <Link href="">{item.name}</Link>
+           </li>
+          ))
+        }
+
+        <button className="nav-btn">Download CV</button>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
